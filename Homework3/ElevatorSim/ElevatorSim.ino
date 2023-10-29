4b5e753eba172376d1118bd5728c6464eea82b58
georgepopescu318 <georgepopescu318@gmail.com>
2023-10-29 21:51:51 +0200

    Elevator 1.2 + comms - readme

--- Homework3/ElevatorSim/ElevatorSim.ino
+++ Homework3/ElevatorSim/ElevatorSim.ino
@@ -1,44 +1,61 @@
-// Here we declare the vector with the pins attached to each button
+// Here I declare the vector with the pins attached to each button
 const int buttonPinArray[3] = {8, 9, 10};
-// Here we declare the vector with the pins attached to the LED from each floor
+// Here I declare the vector with the pins attached to the LED from each floor
 const int ledPinArray[3] = {5, 6, 7};
-// H
+// Here I declare the buzzerPin and the buzzerState function that dictates when and how the buzzer makes sounds
 const int buzzerPin = 13;
 int buzzerState = -1;
+// The sounds that the buzzer make and the time for the arriving sound
+const int arriveSound = 2345;
+const int travellingSound = 250;
+const int arriveSoundTime = 500;
+// The pin of the LED that represents the working condition of the elevator
 const int workingLedPin = 12;
+// The boolean variable that changes the pin state from always HIGH to being HIGH oance every workingLedPeriod ms (500ms is a good number)
 bool workingLedState = 0;
+// I use this variable to determine when the workingLedTime has passed so that that I can flash the LED
 unsigned long workingLedPeriod = 0;
+// Determine how long should the LED be HIGH and LOW
 unsigned long workingLedTime = 500;
+// The following variables will be used in readButtonValue (the debounce function)
+// With reading I detect when the button is being pressed
 bool reading = false;
+// lastReading is used alongside lastDebounceTime to  determine if the noise has ended and a real press ocured
 bool lastReading = false;
 unsigned long lastDebounceTime = 0;
+// buttonState holds the current value of a button, one at a time
 bool buttonState = false;
-bool ledStateArray[3] = {true, true, true};
+// this dictates how much time has the button to be pressed so that the reading can be valid
 unsigned long debounceDelay = 100;
+// I've used only a set of variables for each of the buttons because only the reading of one button at a time can be valide, currentButton represents the button which is in the
+// spotline at a time
 int currentButton = -1;
+// the next floor to which the elevator will go is  kept in nextFloor
 int nextFloor = -1;
+// the current floor at which the elevator is   
 int currentFloor = -1;
+// the state of the LED from the current floor is
 bool currentLedState = true;
+//time that passes from one floor to the other, alongside the period variable for counting that time 
 unsigned long closingTime = 4000;
 unsigned long closingPeriod = 0;
 bool closingState = true;
 
 void setup() {
-  // put your setup code here, to run once:
+  // mainly in the setup I set the pinModes for the buttons, the LEDs and the buzzer
   for (int i = 0; i < 3; i++){
     pinMode(buttonPinArray[i], INPUT_PULLUP);
     pinMode(ledPinArray[i], OUTPUT);
   }
   pinMode(workingLedPin, OUTPUT);
   pinMode(buzzerPin, OUTPUT);
-  
-  Serial.begin(9600);
+  Serial.begin(9600); // for testing 
 }
 void readButtonValue(int buttonPin){
-  
+  // the following function is used to detect the unwanted noise that can appear while pushing the button
   reading = digitalRead(buttonPin);
   if (reading != lastReading) {
-  lastDebounceTime = millis();
+    lastDebounceTime = millis();
   }
   if ((millis() - lastDebounceTime) > debounceDelay) {
   if (reading != buttonState) {
@@ -50,12 +67,13 @@ void readButtonValue(int buttonPin){
     reading = false;
     lastReading = false;
     buttonState = false;
-  }
-  }
+      }
+    }
   }
   lastReading = reading;
 }
 void loop() {
+  // with the for I check from which button did I receive a reading and then check more readings from that button with the above function
   for (int i = 0; i < 3; i++){
       if (digitalRead(buttonPinArray[i]) == 0 && i != currentFloor){
         currentButton = i;
@@ -63,15 +81,17 @@ void loop() {
   }
   readButtonValue(buttonPinArray[currentButton]);
   
+  // if the elevator needs to go to another floor I will use this if to start the millis for the closing time to be accurate
   if (nextFloor != -1 && nextFloor != currentFloor && closingState == false){      
       closingPeriod = millis();
       closingState = true;
   }
+
+  //if half of the time passed the LED will close and after another half the next one will open
   if (closingState == true && (millis() - closingPeriod) > closingTime/2){
       currentLedState = false;
       
     if ((millis() - closingPeriod) > closingTime){
-      Serial.println("aici da");
       if (currentFloor > nextFloor){
         currentFloor -= 1;
       }
@@ -79,9 +99,7 @@ void loop() {
           currentFloor += 1;
       }
       if (currentFloor == nextFloor){
-          //nextFloor = -1;
         buzzerState = 1;
-        Serial.println("a ajuns");
         workingLedState = 1;
         workingLedPeriod = millis();
       }
@@ -90,17 +108,15 @@ void loop() {
       currentLedState = true;
     }
   }
-
-    if (buzzerState == 1){
-      tone(buzzerPin, 2345, 500);
-      buzzerState = -1;
-      //noTone(buzzerPin);
-    }
-
+  // here the buzzer makes the cling sound that tells when the elevator arrived at the desired floor
+  if (buzzerState == 1){
+    tone(buzzerPin, arriveSound, arriveSoundTime);
+    buzzerState = -1;
+  }
+  // the following sequence of code is used to control the LED that represents the working state of the elevator
   if (currentFloor != -1){
      if (currentFloor != nextFloor){
-       // tone(buzzerPin, 100);
-      tone(buzzerPin, 250);
+      tone(buzzerPin, travellingSound);
       if (millis() - workingLedPeriod > workingLedTime){
         workingLedState = !workingLedState;
         workingLedPeriod = millis();
@@ -109,15 +125,12 @@ void loop() {
     }    
     if (currentFloor == nextFloor){
       digitalWrite(workingLedPin, HIGH);
-      //noTone(buzzerPin);
     }
     
   }
   else
     digitalWrite(workingLedPin, LOW);
 
-  //Serial.println(currentFloor);
-  //Serial.println(nextFloor);
-  //digitalWrite(workingLedPin, workingLedState);
+  Serial.println(currentFloor);
   digitalWrite(ledPinArray[currentFloor],currentLedState);
 }
--- README.md
+++ README.md
@@ -25,3 +25,17 @@ Use a separate potentiometer for controlling each color of the RGB LED:Red,Green
 ![Reference photo 3](Homework2/ReferencePhotoHomework2_(2).jpeg)
 
 [__Link to the video__](https://drive.google.com/file/d/1RaZQQeRdR0XG1Q5-4MOC7L5BarMdzbdl/view?usp=sharing)
+
+## [Homework 2](https://github.com/GeorgePopescu318/IntroductionToRobotics/tree/main/Homework2)
+
+Task:
+
+This  assignment  involves  simulating  a  3-floor  elevator  control  system  usingLEDs, buttons, and a buzzer with Arduino. By the end of this task, you will gainexperience in using button state change, implementing debouncing techniques,and coordinating multiple components to represent real-world scenarios.
+ 
+![Reference photo 1](Homework2/ReferencePhotoHomework3_(1).jpeg)
+
+![Reference photo 2](Homework2/ReferencePhotoHomework3_(3).jpeg)
+
+![Reference photo 3](Homework2/ReferencePhotoHomework3_(2).jpeg)
+
+[__Link to the video__]()
